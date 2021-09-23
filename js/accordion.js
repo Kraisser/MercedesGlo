7b0d9ce2cc23638a3b20fa7fd397b696ec5307c1
Kraisser <vgaiko@mail.ru>
2021-09-23 15:03:49 +0300

    firstAdditional

--- js/accordion.js
+++ js/accordion.js
@@ -3,12 +3,37 @@ accordList = document.querySelectorAll(`.feature-sub`);
 
 accordButs.forEach((item) => item.addEventListener(`click`, () => accordShow(item)));
 
-function accordShow(item) {
+// function accordShow(item) {  
+//   if (item.classList.contains(`feature__link_active`)) {
+//     hideElems();
+//     return
+//   }
   
-  accordButs.forEach((itemToActive) => itemToActive.classList.remove(`feature__link_active`));
-  item.classList.add(`feature__link_active`);
+//   hideElems();  
+  
+//   item.classList.add(`feature__link_active`);
+  
+//   let accordTarget = item.parentNode.querySelector(`.feature-sub`);
+//   accordTarget.classList.remove(`hidden`);
+
+//   function hideElems() {
+//     accordButs.forEach((itemToDisActive) => itemToDisActive.classList.remove(`feature__link_active`));
+//     accordList.forEach((itemToHide) => itemToHide.classList.add(`hidden`));
+//   }
+// }
+
+function accordShow(item) {
+  let alreadyActive = item.classList.contains(`feature__link_active`);
+  
+  accordButs.forEach((itemToDisActive) => itemToDisActive.classList.remove(`feature__link_active`));
   accordList.forEach((itemToHide) => itemToHide.classList.add(`hidden`));
+
+  if (alreadyActive) {
+    return
+  }
+  
+  item.classList.add(`feature__link_active`);
+  
   let accordTarget = item.parentNode.querySelector(`.feature-sub`);
   accordTarget.classList.remove(`hidden`);
-  
 }
